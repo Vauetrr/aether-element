f0ef491a3c7f6a6c09f2ec32098cc1884590f1be
Ishaann Cheema <izag0502@gmail.com>
2023-06-16 21:30:42 +1000

    added back scene view

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] private Rigidbody _rb;
@@ -16,22 +17,25 @@ public class PlayerController : MonoBehaviour {
         Move();
     }
 
-    void GatherInput() {
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Debug.Log(_input);
+    void GatherInput() { 
+        Vector2 lsInput = Gamepad.current.leftStick.ReadValue();
+        _input = new Vector3(lsInput.x, 0, lsInput.y);
+        // _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
 
     void Look() {
-        if(_input != Vector3.zero) {
-            var isoBase = Matrix4x4.Rotate(Quaternion.Euler(0, -45, 0));
-            var isoInput = isoBase.MultiplyPoint3x4(_input);
-            var relative = (transform.position + isoInput) - transform.position;
-            var rot = Quaternion.LookRotation(relative, Vector3.up);
-            transform.rotation = rot; // could use lerp (quaternion.rotatetowards)
-        }
+        if(_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = rot; // could use lerp (quaternion.rotatetowards)
     }
 
     void Move() {
-        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+        _rb.MovePosition(transform.position + _input.ToIso() * _input.normalized.magnitude * _speed * Time.deltaTime);
     }
+}
+
+public static class Helpers {
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, -45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
